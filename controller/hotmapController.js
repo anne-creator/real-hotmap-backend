4c02b10f8e952572246ac908fc6472242e8b8096
tonyli <tyli1026@gmail.com>
2023-06-03 14:34:27 -0700

    uberData model updated

--- controller/hotmapController.js
+++ controller/hotmapController.js
@@ -1,10 +1,11 @@
-const asyncHandler = require("express-async-handler")
-
+const asyncHandler = require("express-async-handler");
+const UberData = require("../models/uberData");
 //@desc Get Sample Data
 //@route GET /api/
 const getData = asyncHandler(async (req,res) => {
-    console.log(res)
-    res.status(200).json({message: "this is for test purpose"});
+    const uberData = await UberData.find();
+    console.log(uberData)
+    res.status(200).json(uberData);
 });
 
 
