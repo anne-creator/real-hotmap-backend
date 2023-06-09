9cf4ed6d62de3d192f13326a85725a4d14c8e97e
Zelin <zelintian97@gmail.com>
2023-06-09 17:16:01 -0400

    create weight for density and update mapping of id to lat, long

---
+++ mappingTable/locationIdToLatLong.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+
+const locationId = [];
+const lat = [];
+const long = [];
+const mappingTable = {};
+
+// fs.createReadStream('processed_data-v1.csv')
+//   .pipe(csv())
+//   .on('data', (data) => {
+//     // Extract the desired columns from each row
+//     const id = data['LocationID'];
+//     const latitude = data['Latitude'];
+//     const longitude = data['Longitude'];
+    
+//     locationId.push(id);
+//     lat.push(latitude);
+//     long.push(longitude);
+    
+//     mappingTable[id] = { lat: latitude, long: longitude };
+//   });
+const parseCSV = () => {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream('processed_data-v1.csv')
+      .pipe(csv())
+      .on('data', (data) => {
+        const id = data['LocationID'];
+        const latitude = data['Latitude'];
+        const longitude = data['Longitude'];
+
+        locationId.push(id);
+        lat.push(latitude);
+        long.push(longitude);
+
+        mappingTable[id] = { lat: latitude, long: longitude };
+      })
+      .on('end', () => {
+        resolve(mappingTable);
+      })
+      .on('error', (error) => {
+        reject(error);
+      });
+  });
+};
+
+module.exports = parseCSV;
