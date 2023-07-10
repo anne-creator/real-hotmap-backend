72db5cc780a3dc6a781bf38e004e2dd835361619
Zelin <zelintian97@gmail.com>
2023-07-10 15:28:54 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const createWebSocketServer = require("./websocket/websocket");
 const UberData = require("./models/uberData");
 const cors = require('cors');
 const { runDataGeneration} = require('./simulator');
-
 const { getPickupData } = require('./controller/hotmapController');
 
 app.use(express.json());
